a820eb8c9c2bed1727e3e6d378d7d2321dcca0f7
meghanauser2005 <gnanameghana99@gmail.com>
2025-12-18 17:27:35 +0530

    javascript begin

---
+++ index.js
@@ -0,0 +1,17 @@
+alert("hello world")
+user="hello javascript"
+document.write("<h1>My name is meghana"+"I am an engineer"+user,"<br/>")
+console.log("checking for errors")
+a="apple"
+document.write(a+"is red in colour")
+prize=120
+document.write(a + "prize is" + prize)
+fruits=["orange",155,true]
+document.write(fruits[0])
+console.log(fruits[1])
+student={
+    name:"ajay",
+    city:"hyderabad",
+    pincode:500510
+}
+document.write("<br/>",student.name)
---
+++ variables.js
@@ -0,0 +1,13 @@
+// var,let,const
+var student="Meghana"
+document.write(student)
+var student="vijay"
+document.write(student)
+let score=120
+document.write(score)
+score=150
+document.write(score)
+const player="basketballer"
+document.write(player)
+player="cricketer"
+document.write(player)
