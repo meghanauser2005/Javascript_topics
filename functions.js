b7eb6fa73d135c3a559725d1889aaa9b793ca1f1
meghanauser2005 <gnanameghana99@gmail.com>
2025-12-18 18:04:07 +0530

    intro of func

--- functions.js
+++ functions.js
@@ -1,1 +1,11 @@
 // this file belongs to functions
+// types - normal func,anonymous func,callback func,arrow func
+function userName(){
+    document.write("welcome to functions topics");
+    document.write("<br/> intoduction of function calling")
+}
+userName()
+let b=function (){
+    document.write("anonymous function")
+}
+b()
