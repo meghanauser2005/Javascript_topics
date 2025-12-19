a530bf386a33823dca2f3b72e033a039cea6ebf8
meghanauser2005 <gnanameghana99@gmail.com>
2025-12-19 18:06:40 +0530

    upto return func

---
+++ parameterfuncs.js
@@ -0,0 +1,20 @@
+function adding_numbers(a,b){
+    document.write(a*b)
+}
+adding_numbers(6859,9856)
+// operators
+// +,*,/,%,-,<,>,<=,>=,==,===,!,&&,^,||
+x="10"
+y="10"
+function operator_res(){
+    document.write(x===y)
+}
+operator_res()
+function return_func(){
+    document.write("my name is megha")
+    document.write("today class is abt functions")
+    document.write("today is friday")
+    document.write("tomorrow is saturday")
+    return "hello"
+}
+document.write(return_func())
